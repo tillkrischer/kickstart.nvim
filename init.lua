71707ecfec7e8230e0b724253a5d7ea322a18e64
Till Krischer <till@krischer.io>
2023-03-27 15:14:52 +0200

    null-ls

--- init.lua
+++ init.lua
@@ -91,6 +91,8 @@ require('lazy').setup({
     },
   },
 
+  { 'jose-elias-alvarez/null-ls.nvim' },
+
   {
     -- Autocompletion
     'hrsh7th/nvim-cmp',
@@ -425,6 +427,17 @@ local servers = {
     },
   },
 }
+
+local null_ls = require("null-ls")
+
+null_ls.setup({
+    sources = {
+        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.completion.spell,
+        null_ls.builtins.code_actions.gitsigns,
+        null_ls.builtins.formatting.prettier
+    },
+})
 
 -- Setup neovim lua configuration
 require('neodev').setup()
