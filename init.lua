37f1c5d37af7a60a9ce2aa370d26185c574f8d5a
Till Krischer <till@krischer.io>
2023-03-27 16:23:16 +0200

    vinegar

--- init.lua
+++ init.lua
@@ -179,6 +179,8 @@ require('lazy').setup({
     end,
   },
 
+  { 'tpope/vim-vinegar' },
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
